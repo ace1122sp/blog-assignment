060576f9e1e9cd673b19fea5e4e30c10efedaf37
ace11 <ace11.space22@gmail.com>
2020-08-30 17:03:30 +0200

    clear boilerplate

--- client/index.js
+++ client/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Page from 'client/components/page';
 
 const root = document.getElementById('root');
 
 const App = () => (
-  <>
-    <Page />
-  </>
+  <main>
+    <></>
+  </main>
 );
 
 render(<App />, root);
