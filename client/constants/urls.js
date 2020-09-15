9f71d7e6ac6a93396a3fce5ba0a27fd017c35f58
Alexandar B <ace11.space22@gmail.com>
2020-09-15 21:18:24 +0200

    Update urls.js

--- client/constants/urls.js
+++ client/constants/urls.js
@@ -1,4 +1,3 @@
-export const POSTS_URL =
-  'https://frontend-api-test-nultien.azurewebsites.net/api/BlogPosts';
+export const POSTS_URL = '#'
 
 export const POSTS_BY_CATEGORY_URL = `${POSTS_URL}/GetPostByCategory`;
